b254f1a282ebfd1cb5c038d63b605e46a31ed52e
serhiy.krasovskyy <xhunter74@gmail.com>
2025-06-08 00:07:22 +0300

    Created stub of auth service.

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+
+    constructor(
+        private oidcSecurityService: OidcSecurityService,
+    ) { }
+
+    isLoggedIn() {
+        return this.oidcSecurityService.isAuthenticated$;
+    }
+
+    userData() {
+        return this.oidcSecurityService.userData$;
+    }
+
+    token() {
+        return this.oidcSecurityService.getAccessToken();
+    }
+
+    checkAuth() {
+        return this.oidcSecurityService.checkAuthMultiple();
+    }
+
+    login() {
+        this.oidcSecurityService.authorize();
+    }
+
+    async logout() {
+        this.userProFile = null;
+        await lastValueFrom(this.oidcSecurityService.logoff());
+    }
+}
