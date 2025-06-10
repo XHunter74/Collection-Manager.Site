b8cd98fd42958b7f801e879c77e8c19f8b11ea67
serhiy.krasovskyy <xhunter74@gmail.com>
2025-06-10 13:06:23 +0300

    Manage change of collection Id.

--- src/app/components/manage-collections/manage-collections.component.ts
+++ src/app/components/manage-collections/manage-collections.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-manage-collections',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
     styleUrl: './manage-collections.component.css',
     standalone: false,
 })
-export class ManageCollectionsComponent {}
+export class ManageCollectionsComponent {
+    @Input() collectionId: string | null = null;
+}
