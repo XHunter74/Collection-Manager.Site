64d4f1cdfc4c2d84c04a445ecadefb78af501893
serhiy.krasovskyy <xhunter74@gmail.com>
2025-06-12 18:15:24 +0300

    Pass collection property to item panel.

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { CollectionDto } from '../../models/collection.dto';
 
 @Component({
     selector: 'app-item-component',
@@ -7,5 +8,6 @@ import { Component, Input } from '@angular/core';
     standalone: false,
 })
 export class ItemComponent {
+    @Input() currentCollection: CollectionDto | null = null;
     @Input() itemId: string | null = null;
 }
