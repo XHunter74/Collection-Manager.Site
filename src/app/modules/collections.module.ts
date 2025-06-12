e16dfe2ef772ec9f400c098519dcee14e3e84dbd
serhiy.krasovskyy <xhunter74@gmail.com>
2025-06-12 18:15:05 +0300

    Reorganized.

--- src/app/modules/collections.module.ts
+++ src/app/modules/collections.module.ts
@@ -8,7 +8,7 @@ import { UsersService } from '../services/users.service';
 import { CollectionsComponent } from '../components/collections/collections.component';
 import { SelectCollectionComponent } from '../components/select-collection/select-collection.component';
 import { CollectionsService } from '../services/collections.service';
-import { ItemsPanelComponent } from '../components/items/items-panel.component';
+import { ItemsPanelComponent } from '../components/items-panel/items-panel.component';
 import { CollectionItemsComponent } from '../components/collection-items/collection-items.component';
 import { ItemComponent } from '../components/item/item.component';
 import { DynamicFormComponent } from '../components/dynamic-form/dynamic-form.component';
